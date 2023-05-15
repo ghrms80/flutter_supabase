54aa29230e2276ed3dbb53409013d5fd88449082
jiho <ghrms80@gmail.com>
2023-05-15 22:33:05 +0900

    3 Setup Auth Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_supabase_udemy_re/pages/home_page.dart';
+import 'package:flutter_supabase_udemy_re/pages/start_page.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() async {
@@ -29,7 +30,43 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const HomePage(),
+      home: const AuthPage(),
     );
+  }
+}
+
+class AuthPage extends StatefulWidget {
+  const AuthPage({super.key});
+
+  @override
+  State<AuthPage> createState() => _AuthPageState();
+}
+
+class _AuthPageState extends State<AuthPage> {
+  final SupabaseClient supabase = Supabase.instance.client;
+  User? _user;
+
+  @override
+  void initState() {
+    super.initState();
+    _getAuth();
+  }
+
+  Future<void> _getAuth() async {
+    setState(() {
+      _user = supabase.auth.currentUser;
+    });
+
+    // 사용자의 Auth 상태가 변경되면 home 페이지나 start 페이지로 보내진다.
+    supabase.auth.onAuthStateChange.listen((event) {
+      setState(() {
+        _user = event.session?.user;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _user == null ? const StartPage() : const HomePage();
   }
 }
