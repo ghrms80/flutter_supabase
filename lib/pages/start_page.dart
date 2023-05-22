461aeef668476d03ad05a5be4c491e5a5213d747
jiho <ghrms80@gmail.com>
2023-05-22 21:00:16 +0900

    4 Work on StartPage UI

--- lib/pages/start_page.dart
+++ lib/pages/start_page.dart
@@ -1,13 +1,90 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-class StartPage extends StatelessWidget {
+class StartPage extends StatefulWidget {
   const StartPage({super.key});
 
   @override
+  State<StartPage> createState() => _StartPageState();
+}
+
+class _StartPageState extends State<StartPage> {
+  final SupabaseClient supabase = Supabase.instance.client;
+  final bool _signInLoading = false;
+  final bool _signUpLoading = false;
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  void dispose() {
+    supabase.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: Text("Start Page"),
+        child: SingleChildScrollView(
+          child: Form(
+            key: _formKey,
+            child: Padding(
+              padding: const EdgeInsets.all(20),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Image.network(
+                    "https://seeklogo.com/images/S/supabase-logo-DCC676FFE2-seeklogo.com.png",
+                    height: 150,
+                  ),
+                  const SizedBox(height: 24),
+                  // Email Field
+                  TextFormField(
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return "Field is required";
+                      }
+                      return null;
+                    },
+                    controller: _emailController,
+                    decoration: const InputDecoration(
+                      label: Text('Email'),
+                    ),
+                    keyboardType: TextInputType.emailAddress,
+                  ),
+                  // Password Field
+                  TextFormField(
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return "Field is required";
+                      }
+                      return null;
+                    },
+                    controller: _passwordController,
+                    decoration: const InputDecoration(
+                      label: Text("Password"),
+                    ),
+                    obscureText: true,
+                  ),
+                  const SizedBox(height: 24),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: const Text("Sign In"),
+                  ),
+                  const Divider(),
+                  OutlinedButton(
+                    onPressed: () {},
+                    child: const Text('Sign Up'),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
